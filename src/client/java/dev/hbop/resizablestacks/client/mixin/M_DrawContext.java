3910e684c081f4b24a2d6db09a5f31e8702f884a
HB0P <harrymilne2007@gmail.com>
2025-08-16 16:09:13 +0100

    update to 1.21.8

--- src/client/java/dev/hbop/resizablestacks/client/mixin/M_DrawContext.java
+++ src/client/java/dev/hbop/resizablestacks/client/mixin/M_DrawContext.java
@@ -9,6 +9,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DrawContext.class)
@@ -16,6 +17,25 @@ public abstract class M_DrawContext {
 
     @Shadow @Final private MatrixStack matrices;
     
+    // change item count text to use suffixes
+    @ModifyVariable(
+            method = "drawStackCount",
+            at = @At("HEAD"),
+            ordinal = 0,
+            argsOnly = true
+    )
+    private String changeStackCountText(String value) {
+        if (value == null) return null;
+        if (value.length() <= 3) return value;
+        try {
+            int count = Integer.parseInt(value);
+            return String.valueOf(count);
+        }
+        catch (NumberFormatException e) {
+            return value;
+        }
+    }
+
     // resize item count text
     @Inject(
             method = "drawStackCount",
